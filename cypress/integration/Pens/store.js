d451dc5300a46651c6341d617dacecb56ffc03f4
Кирилл <Kirill08Ciay@gmail.com>
2023-01-30 16:44:04 +0300

    Update store.js

--- cypress/integration/Pens/store.js
+++ cypress/integration/Pens/store.js
@@ -2,22 +2,17 @@ describe('Магазин мебели', function() {
     
     it('Покупка мебели', function(){
         cy.visit('https://testqastudio.me/');
-        cy.get('.post-11342 > .product-inner > .product-thumbnail > .woocommerce-LoopProduct-link > .attachment-woocommerce_thumbnail');
         cy.get('.post-11342 > .product-inner > .product-thumbnail > .woocommerce-LoopProduct-link > .attachment-woocommerce_thumbnail').click();
-        cy.get('.summary > .cart > .product-button-wrapper > .quantity > .increase > svg');
         cy.get('.summary > .cart > .product-button-wrapper > .quantity > .increase > svg').click();
         cy.get('.summary > .cart > .product-button-wrapper > .quantity > .increase > svg').click();
-        cy.get('.summary > .cart > .product-button-wrapper > .single_add_to_cart_button');
         cy.intercept('POST', '/product/**').as('ajax-product');
         cy.intercept('/?wc-ajax=get_refreshed_fragments').as('ajax-reload');
         cy.get('.summary > .cart > .product-button-wrapper > .single_add_to_cart_button').click();
         cy.wait('@ajax-product');
         cy.wait('@ajax-reload');
-        cy.get('.cart-panel-content > .modal-header > .close-account-panel > .razzi-svg-icon > svg');
         cy.get('.cart-panel-content > .modal-header > .close-account-panel > .razzi-svg-icon > svg').click();
         cy.contains('БРОММС Двухместная кровать');
         cy.get('.header-left-items > .site-branding > .logo > .logo-dark').click();
-        cy.get('.post-11337 > .product-inner > .product-thumbnail > .woocommerce-LoopProduct-link > .attachment-woocommerce_thumbnail');
         cy.get('.post-11337 > .product-inner > .product-thumbnail > .woocommerce-LoopProduct-link > .attachment-woocommerce_thumbnail').click();
         cy.contains('КЛЛАРИОН Низкий столик');
         cy.intercept('POST', '/product/**').as('ajax-product');
@@ -34,12 +29,7 @@ describe('Магазин мебели', function() {
         cy.get('#billing_postcode').type('140182');
         cy.get('#billing_phone').type('89656584854');
         cy.get('#billing_email').type('german@dolnikov.ru');
-        cy.get('#place_order');
-        cy.intercept('POST', '/product/**').as('ajax-product');
-        cy.intercept('/?wc-ajax=get_refreshed_fragments').as('ajax-reload');
         cy.get('#place_order').click();
-        cy.wait('@ajax-product');
-        cy.wait('@ajax-reload');
         cy.contains('Ваш заказ принят. Благодарим вас.');
     })
 })
